180728614bd218df8c87727fc19a294e7e4a5c80
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-03-25 20:59:13 +0300

    WORKING avl (2)

--- src/Main.java
+++ src/Main.java
@@ -31,176 +31,160 @@ public class Main {
 
     }
 }
-    class Point {
-        private int x;
-        private int y;
 
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+class Point {
+    private int x;
+    private int y;
 
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    class Segment {
-        private Point p;
-        private Point q;
-
-        Segment(Point p, Point q) {
-            this.p = p;
-            this.q = q;
-        }
-
-        public Point getP() {
-            return p;
-        }
-
-        public Point getQ() {
-            return q;
-        }
-
-        public static Point intersect(Segment s1, Segment s2) {
-            int x = ((s1.getP().getX() * s1.getQ().getY() - s1.getP().getY() * s1.getQ().getX()) * (s2.getP().getX() - s2.getQ().getX()) - (s1.getP().getX() - s1.getQ().getX()) * (s2.getP().getX() * s2.getQ().getY() - s2.getP().getY() * s2.getQ().getX())) / ((s1.getP().getX() - s1.getQ().getX()) * (s2.getP().getY() - s2.getQ().getY()) - (s1.getP().getY() - s1.getQ().getY()) * (s2.getP().getX() - s2.getQ().getX()));
-            int y = ((s1.getP().getX() * s1.getQ().getY() - s1.getP().getY() * s1.getQ().getX()) * (s2.getP().getY() - s2.getQ().getY()) - (s1.getP().getY() - s1.getQ().getY()) * (s2.getP().getX() * s2.getQ().getY() - s2.getP().getY() * s2.getQ().getX())) / ((s1.getP().getX() - s1.getQ().getX()) * (s2.getP().getY() - s2.getQ().getY()) - (s1.getP().getY() - s1.getQ().getY()) * (s2.getP().getX() - s2.getQ().getX()));
-            return null;
-        }
+    public int getX() {
+        return x;
     }
 
-    class Node<T extends Comparable<T>> {
-        T data;
-        Node<T> left;
-        Node<T> right;
+    public int getY() {
+        return y;
+    }
+}
 
-        int height;
+class Segment {
+    private Point p;
+    private Point q;
 
-        public Node(T data) {
-            this.data = data;
-        }
+    Segment(Point p, Point q) {
+        this.p = p;
+        this.q = q;
     }
 
-    class AVLTree<T extends Comparable<T>> {
-        Node<T> root;
+    public Point getP() {
+        return p;
+    }
 
-        private int height(Node<T> node) {
-            return node != null ? node.height : -1;
+    public Point getQ() {
+        return q;
+    }
+
+    public static Point intersect(Segment s1, Segment s2) {
+        int x = ((s1.getP().getX() * s1.getQ().getY() - s1.getP().getY() * s1.getQ().getX()) * (s2.getP().getX() - s2.getQ().getX()) - (s1.getP().getX() - s1.getQ().getX()) * (s2.getP().getX() * s2.getQ().getY() - s2.getP().getY() * s2.getQ().getX())) / ((s1.getP().getX() - s1.getQ().getX()) * (s2.getP().getY() - s2.getQ().getY()) - (s1.getP().getY() - s1.getQ().getY()) * (s2.getP().getX() - s2.getQ().getX()));
+        int y = ((s1.getP().getX() * s1.getQ().getY() - s1.getP().getY() * s1.getQ().getX()) * (s2.getP().getY() - s2.getQ().getY()) - (s1.getP().getY() - s1.getQ().getY()) * (s2.getP().getX() * s2.getQ().getY() - s2.getP().getY() * s2.getQ().getX())) / ((s1.getP().getX() - s1.getQ().getX()) * (s2.getP().getY() - s2.getQ().getY()) - (s1.getP().getY() - s1.getQ().getY()) * (s2.getP().getX() - s2.getQ().getX()));
+        return null;
+    }
+}
+
+class Node<T extends Comparable<T>> {
+    T data;
+    Node<T> left;
+    Node<T> right;
+
+    int height;
+
+    public Node(T data) {
+        this.data = data;
+    }
+}
+
+class AVLTree<T extends Comparable<T>> {
+    Node<T> root;
+
+    private int height(Node<T> node) {
+        if (node == null) {
+            return 0;
+        }
+        if (node.left == null && node.right == null) {
+            return 1;
+        } else if (node.left == null) {
+            return height(node.right) + 1;
+        } else if (node.right == null) {
+            return height(node.left) + 1;
         }
 
-        private void updateHeight(Node<T> node) {
-            int leftChildHeight = height(node.left);
-            int rightChildHeight = height(node.right);
-            node.height = Math.max(leftChildHeight, rightChildHeight) + 1;
-        }
-
-        private int balanceFactor(Node node) {
-            return height(node.right) - height(node.left);
-        }
-
-        private Node<T> rotateRight(Node<T> node) {
-            Node<T> leftChild = node.left;
-
-            node.left = leftChild.right;
-            leftChild.right = node;
-
-            updateHeight(node);
-            updateHeight(leftChild);
-
-            return leftChild;
-        }
-
-        private Node<T> rotateLeft(Node<T> node) {
-            Node<T> rightChild = node.right;
-
-            node.right = rightChild.left;
-            rightChild.left = node;
-
-            updateHeight(node);
-            updateHeight(rightChild);
-
-            return rightChild;
-        }
+        return Math.max(height(node.right) + 1, height(node.left) + 1);
+    }
 
 
-        private Node<T> rebalance(Node<T> node) {
-            int balanceFactor = balanceFactor(node);
+    private int balanceFactor(Node node) {
+        return height(node.right) - height(node.left);
+    }
 
-            // Left-heavy?
-            if (balanceFactor < -1) {
-                if (balanceFactor(node.left) <= 0) {    // Case 1
-                    // Rotate right
-                    node = rotateRight(node);
-                } else {                                // Case 2
-                    // Rotate left-right
-                    node.left = rotateLeft(node.left);
-                    node = rotateRight(node);
-                }
-            }
+    private Node<T> rotateRight(Node<T> node) {
+        Node<T> leftChild = node.left;
 
-            // Right-heavy?
-            if (balanceFactor > 1) {
-                if (balanceFactor(node.right) >= 0) {    // Case 3
-                    // Rotate left
-                    node = rotateLeft(node);
-                } else {                                 // Case 4
-                    // Rotate right-left
-                    node.right = rotateRight(node.right);
-                    node = rotateLeft(node);
-                }
-            }
+        node.left = leftChild.right;
+        leftChild.right = node;
 
-            return node;
-        }
+        return leftChild;
+    }
 
-        Node insert(Node<T> rootNode, T value) {
-            if (rootNode == null) {
-                rootNode = new Node<>(value);
-                return rootNode;
-            }
+    private Node<T> rotateLeft(Node<T> node) {
+        Node<T> rightChild = node.right;
 
-            // Otherwise, traverse the tree to the left or right depending on the key
-            if (value.compareTo(rootNode.data) < 0) {
-                rootNode.left = insert(rootNode.left, value);
-            } else if (value.compareTo(rootNode.data) > 0) {
-                rootNode.right = insert(rootNode.right, value);
+        node.right = rightChild.left;
+        rightChild.left = node;
+
+        return rightChild;
+    }
+
+
+    private Node<T> rebalance(Node<T> node) {
+        int balanceFactor = balanceFactor(node);
+
+        if (balanceFactor < -1) {
+            if (balanceFactor(node.left) <= 0) {
             } else {
-                throw new IllegalArgumentException("AVL Tree already contains a node with this key");
+                node.left = rotateLeft(node.left);
             }
+            node = rotateRight(node);
+        }
 
-            updateHeight(rootNode);
+        if (balanceFactor > 1) {
+            if (balanceFactor(node.right) < 0) {
+                node.right = rotateRight(node.right);
+            }
+            node = rotateLeft(node);
+        }
 
-            rootNode = rebalance(rootNode);
+        return node;
+    }
 
+    Node insert(Node<T> rootNode, T value) {
+        if (rootNode == null) {
+            rootNode = new Node<>(value);
             return rootNode;
         }
 
-        Node insert(T value) {
-            Node<T> rootNode = this.root;
-            if (rootNode == null) {
-                rootNode = new Node<>(value);
-                this.root = rootNode;
-                return rootNode;
-            }
+        if (value.compareTo(rootNode.data) < 0) {
+            rootNode.left = insert(rootNode.left, value);
+        } else {
+            rootNode.right = insert(rootNode.right, value);
+        }
 
-            // Otherwise, traverse the tree to the left or right depending on the key
-            if (value.compareTo(rootNode.data) <= -1) {
-                rootNode.left = insert(rootNode.left, value);
-            } else if (value.compareTo(rootNode.data) >= 1) {
-                rootNode.right = insert(rootNode.right, value);
-            } else {
-                throw new IllegalArgumentException("AVL Tree already contains a node with this key");
-            }
 
-            updateHeight(rootNode);
+        rootNode = rebalance(rootNode);
 
-            this.root = rebalance(rootNode);
+        return rootNode;
+    }
 
+    Node insert(T value) {
+        Node<T> rootNode = this.root;
+        if (rootNode == null) {
+            rootNode = new Node<>(value);
+            this.root = rootNode;
             return rootNode;
         }
+
+        if (value.compareTo(rootNode.data) < 0) {
+            rootNode.left = insert(rootNode.left, value);
+        } else {
+            rootNode.right = insert(rootNode.right, value);
+        }
+
+
+        this.root = rebalance(rootNode);
+
+        return rootNode;
+    }
 
 }
